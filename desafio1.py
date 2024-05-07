f7e9f4efc1a2dde5acedfd2c9ea062435c901a4d
Sofia Daria Sguera <137823381+sofidar@users.noreply.github.com>
2024-05-07 16:51:32 -0300

    Update desafio1.py

--- desafio1.py
+++ desafio1.py
@@ -1,19 +1,42 @@
 import random
 
 jugadoras_arg=[
-    ("Agustina Albertario", 19), ("Cristina Cosentino", 13), ("Julieta Jankunas", 28),
-    ("Valentina Marcucci", 31), ("Rocío Sánchez Moccia", 17), ("Victoria Sauze Valdez", 18),
-    ("Eugenia Trinchinetti", 22), ("Valentina Costa Biondi", 32), ("Agustina Gorzelany", 3),
-    ("María José Granatto", 15), ("Sofia Toccalino", 2), ("Agostina Alonso", 5),
-    ("Valentina Raposo", 4), ("Clara Barberi", 14), ("Delfina Thome", 4), ("Sofía Cairo", 8),
+    ("Agustina Albertario", 19), 
+    ("Cristina Cosentino", 13), 
+    ("Julieta Jankunas", 28),
+    ("Valentina Marcucci", 31), 
+    ("Rocío Sánchez Moccia", 17), 
+    ("Victoria Sauze Valdez", 18),
+    ("Eugenia Trinchinetti", 22), 
+    ("Valentina Costa Biondi", 32), 
+    ("Agustina Gorzelany", 3),
+    ("María José Granatto", 15), 
+    ("Sofia Toccalino", 2), 
+    ("Agostina Alonso", 5),
+    ("Valentina Raposo", 4), 
+    ("Clara Barberi", 14), 
+    ("Delfina Thome", 4), 
+    ("Sofía Cairo", 8),
     ("María Pilar Campoy", 23)
 ]
 
 jugadoras_aus=[
-    ("Ashlee Wells", 5), ("Jocelyn Bartram", 19), ("Rachel Lynch", 27), ("Sophie Taylor", 1),
-    ("Karri McMahon", 11), ("Edwina Bone", 13), ("Kaitlin Nobbs", 15), ("Madison Fitzpatrick", 10),
-    ("Amy Lawton", 4), ("Karri Somerville", 20), ("Kate Jenner", 22), ("Georgia Wilson", 8),
-    ("Lily Brazel", 9), ("Greta Hayes", 12), ("Stephanie Kershaw", 14), ("Jane Claxton", 18),
+    ("Ashlee Wells", 5), 
+    ("Jocelyn Bartram", 19), 
+    ("Rachel Lynch", 27), 
+    ("Sophie Taylor", 1),
+    ("Karri McMahon", 11), 
+    ("Edwina Bone", 13), 
+    ("Kaitlin Nobbs", 15), 
+    ("Madison Fitzpatrick", 10),
+    ("Amy Lawton", 4), 
+    ("Karri Somerville", 20), 
+    ("Kate Jenner", 22), 
+    ("Georgia Wilson", 8),
+    ("Lily Brazel", 9), 
+    ("Greta Hayes", 12), 
+    ("Stephanie Kershaw", 14), 
+    ("Jane Claxton", 18)
 ]
 
 
